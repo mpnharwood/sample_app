e5d295dcca6636a74448a49bdd5b9c9a15d09c78
Matt Harwood <mpn.harwood@googlemail.com>
2013-01-10 22:57:15 +0000

    Clean up RSpec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,21 +6,20 @@ describe "User Pages" do
 	describe "signup page" do
 		before { visit signup_path }
 
-		it { should have_selector('h1',		text: 'Sign up') }
-		it { should have_selector('title', 	text: full_title('Sign up')) }
+		it { should have_heading('Sign up') }
+		it { should have_title('Sign up') }
 	end
 
 	describe "profile page" do
 		let(:user) {FactoryGirl.create(:user) }	# Code to make a user variable
 		before { visit user_path(user) }
 
-		it { should have_selector('h1', 	text: user.name)}
-		it { should have_selector('title', 	text: user.name)}
+		it { should have_heading(user.name)}
+		it { should have_title(user.name)}
 	end
 
 	describe "signup" do
 		before{visit signup_path}
-
 		let(:submit) { "Create my account"}
 
 		describe "with invalid information" do
@@ -31,19 +30,13 @@ describe "User Pages" do
       		describe "after submission" do
       			before { click_button submit }
 
-      			it {should have_selector('title', text: 'Sign up')}
+      			it {should have_title('Sign up')}
       			it {should have_content('error')}
       		end
     	end
 
 		describe "with valid information" do
-			before do
-				visit signup_path
-				fill_in "Name", 			with: "Example User"
-				fill_in "Email",				with: "user@example.com"
-				fill_in "Password",			with: "foobar"
-				fill_in "Confirmation",		with: "foobar"
-			end
+			before {valid_signup}
 			it "should create a user" do
 				expect {click_button submit}.to change(User, :count).by(1)
 			end
@@ -52,7 +45,7 @@ describe "User Pages" do
 				before { click_button submit}
 				let(:user) {User.find_by_email('user@example.com')}
 
-				it {should have_selector('title', text: user.name)}
+				it {should have_title(user.name)}
 				it {should have_selector('div.alert.alert-success', text: "Welcome")}
 				it {should have_link('Sign out')}
 			end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,23 @@ RSpec::Matchers.define :have_error_message do |message|
 		page.should have_selector('div.alert.alert-error', text: message)
 	end
 end
+
+def valid_signup
+	visit signup_path
+	fill_in "Name", 			with: "Example User"
+	fill_in "Email",			with: "user@example.com"
+	fill_in "Password",			with: "foobar"
+	fill_in "Confirmation",		with: "foobar"
+end
+
+RSpec::Matchers.define :have_title do |title|
+	match do |page|
+		page.should have_selector 'title', text: full_title(title)
+	end
+end
+
+RSpec::Matchers.define :have_heading do |heading|
+	match do |page|
+		page.should have_selector('h1',       text: heading)
+	end
+end
