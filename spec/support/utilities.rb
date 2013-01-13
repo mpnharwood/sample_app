66fa677e4a923b670a1c7c6691809df0b582dbf2
Matt Harwood <mpn.harwood@googlemail.com>
2013-01-13 21:55:31 +0000

    Finish user edit, update, index and destrpy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -20,6 +20,15 @@ def valid_signup
 	fill_in "Confirmation",		with: "foobar"
 end
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
 RSpec::Matchers.define :have_title do |title|
 	match do |page|
 		page.should have_selector 'title', text: full_title(title)
